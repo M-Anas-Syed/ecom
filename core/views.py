54eab34ee8532e5fc447cefd4452c85c530556b4
“Anas <2436370s@student.gla.ac.uk>
2021-06-23 19:07:57 +0300

    cart v.1.0

--- core/views.py
+++ core/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Item
 
 
-def item_list(request):
-    context = {
-        'items': Item.objects.all()
-    }
-    return render(request, "home-page.html", context)
+class HomeView(ListView):
+    model = Item
+    template_name = "home.html"
 
-def product(request):
-    return render(request,"product-page.html")
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "product.html"
+
 
 def checkout(request):
-    return render(request,"checkout-page.html")
+    return render(request, "checkout.html")
